f064db36b431eb39b6e1dece4b608fc1567b0cfd
EdsonJunio <69820618+EdsonJunio@users.noreply.github.com>
2024-05-31 00:24:14 -0300

    FEAT: Atualizar o AuthController com validação e implementar testes
    Este commit introduz a validação de e-mail e senha no método postAuthenticate do AuthController. Também diferencia entre os fornecedores “utilizador” e “comerciante” e trata a entrada inválida. Além disso, foram incluídos casos de teste para erros de provedor errado e credenciais erradas.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Merchant;
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function postAuthenticate(string $response)
+    public function postAuthenticate(Request $request, string $provider)
     {
-        return response('O reponse escolhido é ' . $response . 200);
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+
+        $providers = ['user', 'merchant'];
+
+        if (!in_array($provider, $providers)) {
+            return response()->json(['errors' => ['main' => 'Wrong provider']], 422);
+        }
+
+        $provider = $this->getProvider($provider);
+
+        $model = $provider->where('email', '=', $request->input('email'))->first();
+
+        if(!$model) {
+            return response()->json(['errors' => ['main' => 'Wrong credentials']], 401);
+        }
+
+        return 'O response escolhido é ' . $provider;
+    }
+
+    public function getProvider(string $provider): Authenticatable
+    {
+        if ($provider == 'user') {
+            return new User();
+        }else if ($provider == 'merchant') {
+            return new Merchant();
+        } else {
+            throw new \InvalidArgumentException('Invalid provider');
+        }
     }
 }
